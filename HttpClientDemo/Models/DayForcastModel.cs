da09d412dad559ba63fce9b865142fd2b5b00b51
Ryan Fiorini <rfiorini@bankmobile.com>
2021-05-06 08:38:47 -0400

    Add project files.

---
+++ HttpClientDemo/Models/DayForcastModel.cs
@@ -0,0 +1,19 @@
+﻿using System.Text.Json.Serialization;
+
+namespace HttpClientDemo.Models
+{
+    public class DayForcastModel
+    {
+        [JsonPropertyName("weather_state_name")]
+        public string WeatherStateName { get; set; }
+
+        [JsonPropertyName("applicable_date")]
+        public string ApplicableDate { get; set; }
+
+        [JsonPropertyName("min_temp")]
+        public float MinTemp { get; set; }
+
+        [JsonPropertyName("max_temp")]
+        public float MaxTemp { get; set; }
+    }
+}
---
+++ HttpClientDemo/Models/WeatherForcastModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Text.Json.Serialization;
+
+namespace HttpClientDemo.Models
+{
+
+    public class WeatherForcastModel
+    {
+        public DayForcastModel[] Consolidated_weather { get; set; }
+
+        [JsonPropertyName("sun_rise")]
+        public DateTime SunRise { get; set; }
+
+        [JsonPropertyName("sun_set")]
+        public DateTime SunSet { get; set; }
+
+        public string Title { get; set; }
+        public string Timezone { get; set; }
+    }
+}
